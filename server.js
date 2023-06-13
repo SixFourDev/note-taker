19c139428947162b436822d242898106f51d6600
Shane <sb_94@yahoo.com>
2023-06-13 12:06:45 -0700

    added port number, set up routes to handle requests, and app.listen

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Set up Express.js and require necessary modules
 const express = require('express');
 const path = require('path');
+// Port number
+const port = 3000;
 
 // Create an instance of the Express app
 const app = express();
@@ -8,3 +10,17 @@ const app = express();
 // Define middleware to handle JSON and static file serving
 app.use(express.json());
 app.use(express.static('public'));
+
+// Set up routes to handle requests
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// Starts the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
