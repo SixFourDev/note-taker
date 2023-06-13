cc8156367a2edf6fed2a1b9f1fa332062c6e8fe5
Shane <sb_94@yahoo.com>
2023-06-13 15:55:29 -0700

    handle get and post requests for /api/notes

--- server.js
+++ server.js
@@ -20,6 +20,21 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// Create a new array to store the notes
+let notes = [];
+
+// Handle get requests and returning the notes array as a JSON
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+// Handle receiving new notes, pushing them to the notes array and returning new note as json
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  notes.push(newNote);
+  res.json(newNote);
+});
+
 // Starts the server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
