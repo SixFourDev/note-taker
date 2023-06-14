ac9503c52f0188f8c5fa78d15764db6c858b79d2
Shane <sb_94@yahoo.com>
2023-06-14 14:00:41 -0700

    added comments in server.js file

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+// Import Express module
 const express = require('express');
+// Import path module
 const path = require('path');
+// Import htmlRoutes module
 const htmlRoutes = require('./routes/htmlRoutes');
+// Import apiRoutes module
 const apiRoutes = require('./routes/apiRoutes');
 
+// Create instance of the express app
 const app = express();
+// Defines Port number
 const PORT = process.env.PORT || 3000;
 
+// Add middleware to app
 app.use(express.json());
 app.use(express.static('public'));
 
+// Grabs the router middleware for the / path
 app.use('/', htmlRoutes);
+// Grabs the router middleware for the /api path
 app.use('/api', apiRoutes);
 
+// Starts the server and listens for requests
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
