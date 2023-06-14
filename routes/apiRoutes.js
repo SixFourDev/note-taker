09fbc363aa76e642c361431560366f74e3b65359
Shane <sb_94@yahoo.com>
2023-06-14 13:53:46 -0700

    added comments to apiRoutesfile

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,26 @@
+// Import express module
 const express = require('express');
+// Created new router using the express router
 const router = express.Router();
 
+// Initialized an empty array to store notes
 let notes = [];
 
+// Defined GET route handler for the /notes endpoint
 router.get('/notes', (req, res) => {
+  // Send notes array as JSON response
   res.json(notes);
 });
 
+// Defined POST route for the /notes endpoint
 router.post('/notes', (req, res) => {
+  // Gets note data from the request body
   const newNote = req.body;
+  // Adds new note to the notes array
   notes.push(newNote);
+  // Sends new note as a JSON response
   res.json(newNote);
 });
 
+// Exports the router object avaiable to use in other files
 module.exports = router;
